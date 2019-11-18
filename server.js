07c33b0aec24f414168af76e4abab1591c68dd0c
Logan <wislermichel@gmail.com>
2019-11-18 00:28:58 -0500

    setting up the server page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+var app = express();
+const exphbs = require("express-handlebars");
+const cheerio =require("cheerio");
+const axios = require("axios");
+var path = require("path");
+
+
+var PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function(){
+    console.log(`Server is listening on port ${PORT}:`);
+})
