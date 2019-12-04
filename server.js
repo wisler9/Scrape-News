09ba2b7d74756a6ccb94fdcc68efdde9adc40bdc
Logan <wislermichel@gmail.com>
2019-12-04 08:09:57 -0500

    Initial upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 var app = express();
 const exphbs = require("express-handlebars");
 const cheerio =require("cheerio");
+var mongoose = require("mongoose");
 const axios = require("axios");
 var path = require("path");
 
@@ -10,6 +11,7 @@ var PORT = process.env.PORT || 3000;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 
 app.listen(PORT, function(){
     console.log(`Server is listening on port ${PORT}:`);
